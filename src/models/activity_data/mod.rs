9015194594cb54da9d48949eb87213ab4f909eeb
AmokDev <114482884+AmokDev@users.noreply.github.com>
2025-03-18 10:43:51 +0300

    Delete src directory

--- src/models/activity_data/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod activity_flag;
-pub mod activity_type;
-pub mod asset;
-pub mod button;
-pub mod emoji;
-pub mod party;
-pub mod secret;
-pub mod timestamp;
--- src/models/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod activity;
-pub mod activity_data;
-pub mod client;
-pub mod error;
